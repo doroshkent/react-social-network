d4c9656f6e833408bc6072e5c25e0b2d77c246da
Anastasiya Darashkevich <doroshkent@Nikitas-MacBook-Pro.local>
2023-03-07 17:09:04 +0100

    Add ref to Posts and Dialogues

--- src/components/Dialogues/Dialogues.js
+++ src/components/Dialogues/Dialogues.js
@@ -1,3 +1,4 @@
+import React from "react";
 import style from './Dialogues.module.css'
 import DialogueItem from "./DialogueItem/DialogueItem";
 import MessageItem from "./MessageItem/MessageItem";
@@ -11,6 +12,13 @@ const Dialogues = (props) => {
         return <MessageItem message={message.messageText} isSent={message.isSent}/>
     })
 
+    let messageText = React.createRef();
+
+    let sendMessage = () => {
+        let message = messageText.current.value;
+        alert(message);
+    }
+
     return (
         <div className={style.dialogues}>
             <div className={style.dialogueList}>
@@ -18,6 +26,8 @@ const Dialogues = (props) => {
             </div>
             <div className={style.messageList}>
                 { messageItems }
+                <textarea ref = { messageText } className={style.messageTextarea}></textarea>
+                <button onClick={ sendMessage } className={style.sendMessageButton}>Send message</button>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,15 +7,22 @@ const MyPosts = (props) => {
         return <Post message={post.message} likeCount={post.likesCount}/>
     })
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
+
     return (
         <div className={style.postsBlock}>
             <h2>My posts</h2>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={ newPostElement }></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={ addPost }>Add post</button>
                 </div>
             </div>
             <div className={style.posts}>
